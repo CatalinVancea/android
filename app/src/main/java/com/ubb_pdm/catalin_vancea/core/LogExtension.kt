51d9304162ecd41f3386dfdce4689d89e842f7d6
Catalin Vancea <vancea_catalin24@yahoo.ro>
2020-12-06 13:55:55 +0200

    lab3

--- app/src/main/java/com/ubb_pdm/catalin_vancea/core/LogExtension.kt
+++ app/src/main/java/com/ubb_pdm/catalin_vancea/core/LogExtension.kt
@@ -3,5 +3,5 @@ package com.ubb_pdm.catalin_vancea.core
 val Any.TAG: String
     get() {
         val tag = javaClass.simpleName
-        return if (tag.length <= 23) tag else tag.substring(0, 23)
+        return if (tag.length <= 23) tag else tag.substring(0, 23) 
     }
